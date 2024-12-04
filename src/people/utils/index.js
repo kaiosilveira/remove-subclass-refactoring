2bb61a78b442b6e70cf0ceebd18c1394f3d3e8cc
Kaio Silveira <silveira.kaio@icloud.com>
2024-12-04 14:02:50 +0000

    simplify implementation of `loadFromInput`

--- src/people/utils/index.js
+++ src/people/utils/index.js
@@ -1,9 +1,5 @@
 import { createPerson } from '../factory';
 
 export function loadFromInput(data) {
-  const result = [];
-  data.forEach(aRecord => {
-    return result.push(createPerson(aRecord));
-  });
-  return result;
+  return data.map(aRecord => createPerson(aRecord));
 }
