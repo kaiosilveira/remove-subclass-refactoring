f40088cb101619a03deb13c0166ed2bc3c1ab1d5
Kaio Silveira <silveira.kaio@icloud.com>
2024-12-04 14:00:00 +0000

    update `loadFromInput` to use `createPerson`

--- src/people/utils/index.js
+++ src/people/utils/index.js
@@ -5,19 +5,7 @@ import { Person } from '../person/index.js';
 export function loadFromInput(data) {
   const result = [];
   data.forEach(aRecord => {
-    let p;
-    switch (aRecord.gender) {
-      case 'M':
-        p = new Male(aRecord.name);
-        break;
-      case 'F':
-        p = new Female(aRecord.name);
-        break;
-      default:
-        p = new Person(aRecord.name);
-        break;
-    }
-    return result.push(p);
+    return result.push(createPerson(aRecord));
   });
   return result;
 }
