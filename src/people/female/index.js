9ce8b40742c07009cf5441255f3750aa27a3444b
Kaio Silveira <silveira.kaio@icloud.com>
2024-12-04 15:43:49 +0000

    remove `Female` subclass

--- src/people/female/index.js
+++
@@ -1,7 +0,0 @@
-import { Person } from '../person/index.js';
-
-export class Female extends Person {
-  get genderCode() {
-    return 'F';
-  }
-}
--- src/people/female/index.test.js
+++
@@ -1,8 +0,0 @@
-import { Female } from '.';
-
-describe('Female', () => {
-  it('should have genderCode as F', () => {
-    const female = new Female('Jane Doe');
-    expect(female.genderCode).toBe('F');
-  });
-});
